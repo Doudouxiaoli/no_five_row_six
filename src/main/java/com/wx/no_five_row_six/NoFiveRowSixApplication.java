103ecd9371a5d1374536f30978d5f590d4373ea3
douxiaoli <douxiaoli.medlive.cn>
2020-01-13 13:46:39 +0800

    init project

---
+++ src/main/java/com/wx/no_five_row_six/NoFiveRowSixApplication.java
@@ -0,0 +1,13 @@
+package com.wx.no_five_row_six;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoFiveRowSixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoFiveRowSixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wx/no_five_row_six/NoFiveRowSixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.no_five_row_six;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoFiveRowSixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
